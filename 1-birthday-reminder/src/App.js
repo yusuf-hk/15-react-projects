c356514c2fab12a63a508096e06b98691e4101d7
Yusuf <yusufhkaya31@gmail.com>
2021-01-10 23:26:48 +0100

    Ability to remove one person or clear all list

--- 1-birthday-reminder/src/App.js
+++ 1-birthday-reminder/src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import List from "./List";
+import data from "./data";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+function App()
+{
+    const[people, setPeople] = useState(data)
+    const[count, setCount] = useState(1)
+
+
+    const removePerson = () => {
+        setCount(count + 1)
+        let currentCount = count
+        setPeople(people.filter(({ id }) => id !== currentCount))
+    }
+
+    return (
+        <main>
+            <section className="container">
+                <h3>{people.length} birthdays today</h3>
+                <List people={people}/>
+                <button onClick={() => removePerson()}> Remove one person </button>
+                <button onClick={() => setPeople([])}> Clear all </button>
+            </section>
+        </main>);
 }
 
 export default App;
