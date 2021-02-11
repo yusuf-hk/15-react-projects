da65589415d5cb7defbd45d39743ca258036f3d7
Yusuf <yusufhkaya31@gmail.com>
2021-02-11 16:18:53 +0100

    did something

---
+++ 5-menu/src/App.js
@@ -0,0 +1,31 @@
+import React, {useState} from 'react';
+import Menu from './Menu';
+import Categories from './Categories';
+import items from './data';
+
+function App() {
+    const [menuItems, setMenuItems] = useState(items)
+    const [categories, setCategories] = useState([])
+
+    const filterItems = (category) => {
+        if(category === "al")
+        const newItems = items.filter((item) => item.category
+        === category);
+        setMenuItems(newItems);
+    };
+
+    return (
+        <main>
+            <section className="menu section">
+                <div className="title">
+                    <h2>our menu</h2>
+                    <div className="underline"></div>
+                </div>
+                <Categories/>
+                <Menu items={menuItems}/>
+            </section>
+        </main>
+    );
+}
+
+export default App;
